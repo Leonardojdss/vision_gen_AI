5354d795aa39881ed578f62f62205b51e9a879c4
leonardo <leonardo.jsilva@telefonica.com>
2025-04-09 00:13:14 -0300

    fixed

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 from dotenv import load_dotenv
-from src.use_case.analyze_image import AnalyzeImageUseCase
+from use_case.analyze_image import AnalyzeImageUseCase
 import os
 from io import BytesIO
 
